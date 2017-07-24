9cc5729d205f5be0c642fb29ad21d30a87001dac
Asme123 <30399109+Asme123@users.noreply.github.com>
2017-07-24 13:00:59 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,19 @@
+package JavaAssignment1;
+
+public class Sum {
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b=11;
+		int sum = a-(-b);
+		System.out.println("Sum of the Numbers = " + sum);
+	}
+}
+	
+
+	
+
+	
+
+	
+
